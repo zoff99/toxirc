49f026c436c3dbfdf730878bb8b1599d4177712e
zoff99 <zoff99@users.noreply.github.com>
2022-05-07 10:33:30 +0200

    log exact timestamp.

--- src/logging.c
+++ src/logging.c
@@ -3,15 +3,31 @@
 #include "settings.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
+#include <string.h>
+#include <sys/time.h>
+#include <time.h>
 
 void debug(const char *fmt, ...) {
     if (!settings.verbose) {
         return;
     }
 
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    struct tm tm = *localtime(&tv.tv_sec);
+
+    const size_t len = 26 + 5 + strlen(fmt) + 2;
+    char *buf = calloc(1, len);
+    buf[28] = ' ';
+    snprintf(buf, len, "%04d-%02d-%02d %02d:%02d:%02d.%06ld [_] %s", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday,
+             tm.tm_hour, tm.tm_min, tm.tm_sec, tv.tv_usec, fmt);
+
     va_list ap;
     va_start(ap, fmt);
-    vprintf(fmt, ap);
+    vprintf(buf, ap);
     va_end(ap);
+
+    free(buf);
 }
